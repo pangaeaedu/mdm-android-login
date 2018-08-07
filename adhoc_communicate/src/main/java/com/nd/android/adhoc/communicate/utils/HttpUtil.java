03188a6aaa134f264421b3b4bd55e1c705b581d1
黄艺凯 <807851@nd.com>
2018-08-07 17:12:29 +0800

    1、httputil 增加 get 方法（后续再对接到 adhoc_basic_net 模块）
    Change-Id: Ied7950d58cb01bb92b6b202b848b30bc6eb957a7

--- adhoc_communicate/src/main/java/com/nd/android/adhoc/communicate/utils/HttpUtil.java
+++ adhoc_communicate/src/main/java/com/nd/android/adhoc/communicate/utils/HttpUtil.java
@@ -3,13 +3,24 @@ package com.nd.android.adhoc.communicate.utils;
 import com.nd.android.adhoc.basic.log.Logger;
 import com.nd.android.adhoc.communicate.impl.MdmTransferFactory;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -45,4 +56,44 @@ public class HttpUtil {
         }
     }
 
+
+    public static String get(String url, Map<String, Object> params) {
+        String ret = null;
+        if (params != null && !params.isEmpty()) {
+            List<NameValuePair> pairs = new ArrayList<NameValuePair>(params.size());
+            for (String key : params.keySet()) {
+                pairs.add(new BasicNameValuePair(key, params.get(key).toString()));
+            }
+            try {
+                url += "?" + EntityUtils.toString(new UrlEncodedFormEntity(pairs), HTTP.UTF_8);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        ret = get(url);
+        return ret;
+    }
+
+    private static String get(String uri) {
+        Logger.d(TAG, "get json from : " + uri);
+        String result = null;
+        try {
+            HttpGet httpGet = new HttpGet(uri);
+            HttpResponse httpResponse = new DefaultHttpClient().execute(httpGet);
+            int statusCode = httpResponse.getStatusLine().getStatusCode();
+            if (statusCode != HttpStatus.SC_OK) {
+                httpGet.abort();
+                Logger.e(TAG, "HttpClient, error status code :" + statusCode);
+            } else {
+                HttpEntity entity = httpResponse.getEntity();
+                if (entity != null) {
+                    result = EntityUtils.toString(entity, HTTP.UTF_8);
+                    return result;
+                }
+            }
+        } catch (IOException e) {
+            Logger.e(TAG, "request failed : " + e.toString());
+        }
+        return result;
+    }
 }
