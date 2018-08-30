917a3b41571fe34db048cb0ea3863ffa2924d716
winnyang <abird1983@163.com>
2018-08-30 10:27:32 +0800

    连接与未连接的处理都在workthread中处理
    Change-Id: I6dc8777e5f72007454f2b396bf8639f6224b6270

--- adhoc_communicate/src/main/java/com/nd/android/adhoc/communicate/push/listener/IPushConnectListener.java
+++ adhoc_communicate/src/main/java/com/nd/android/adhoc/communicate/push/listener/IPushConnectListener.java
@@ -1,12 +1,16 @@
 package com.nd.android.adhoc.communicate.push.listener;
 
+import android.support.annotation.WorkerThread;
+
 /**
  * Created by HuangYK on 2018/8/7.
  */
 
 public interface IPushConnectListener {
 
+    @WorkerThread
     void onConnected();
 
+    @WorkerThread
     void onDisconnected();
 }
