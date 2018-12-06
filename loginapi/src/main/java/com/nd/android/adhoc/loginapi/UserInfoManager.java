69a02fcc6141a38f964620d71831319e64b78a48
杨宏群 <112977@nd.com>
2018-12-06 15:53:12 +0800

    增加CmdUserID转发功能
    Change-Id: I7ccf54be7ce956b494584a3bb433eec85da19851

---
+++ loginapi/src/main/java/com/nd/android/adhoc/loginapi/UserInfoManager.java
@@ -0,0 +1,40 @@
+package com.nd.android.adhoc.loginapi;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class UserInfoManager {
+    private static final UserInfoManager ourInstance = new UserInfoManager();
+
+    public static UserInfoManager getInstance() {
+        return ourInstance;
+    }
+
+    private List<ICdmUserIDListener> mCdmUserIDListeners = new CopyOnWriteArrayList<>();
+
+    private UserInfoManager() {
+    }
+
+    public void onCmdUserIDUpdate(String pUserID){
+        for (ICdmUserIDListener listener : mCdmUserIDListeners) {
+            listener.onCdmUserIDUpdate(pUserID);
+        }
+    }
+
+    public void addUserIDListener(ICdmUserIDListener pListener){
+        if(pListener == null || mCdmUserIDListeners.contains(pListener)){
+            return;
+        }
+
+        mCdmUserIDListeners.add(pListener);
+    }
+
+    public void removeUserIDListener(ICdmUserIDListener pListener){
+        if(pListener == null || !mCdmUserIDListeners.contains(pListener)){
+            return;
+        }
+
+        mCdmUserIDListeners.remove(pListener);
+    }
+
+}
