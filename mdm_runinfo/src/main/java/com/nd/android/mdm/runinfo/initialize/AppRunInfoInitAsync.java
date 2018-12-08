3a01b44bdce754b98d9fff3130b183a97f74b3a8
黄艺凯 <807851@nd.com>
2018-12-08 19:03:37 +0800

    1、AppRunInfoInitAsync 补充 @Service 的注解
    Change-Id: I2cd5854c2f7a3bde5bda24bd04649fd599db3109

--- mdm_runinfo/src/main/java/com/nd/android/mdm/runinfo/initialize/AppRunInfoInitAsync.java
+++ mdm_runinfo/src/main/java/com/nd/android/mdm/runinfo/initialize/AppRunInfoInitAsync.java
@@ -3,16 +3,16 @@ package com.nd.android.mdm.runinfo.initialize;
 import com.nd.android.adhoc.basic.common.AdhocBasicConfig;
 import com.nd.android.adhoc.basic.frame.api.initialization.AdhocAppInitAsyncAbs;
 import com.nd.android.mdm.runinfo.business.task.AppRunInfoTaskModule;
+import com.nd.sdp.android.serviceloader.annotation.Service;
 
 /**
  * Created by HuangYK on 2018/12/8.
  */
-
+@Service(AdhocAppInitAsyncAbs.class)
 public class AppRunInfoInitAsync extends AdhocAppInitAsyncAbs {
 
     @Override
     public boolean doInitAsync() {
-
         AppRunInfoTaskModule.getInstance().init(AdhocBasicConfig.getInstance().getAppContext());
         return true;
     }
