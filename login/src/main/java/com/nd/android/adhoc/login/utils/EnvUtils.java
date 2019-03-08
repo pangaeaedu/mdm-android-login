237a5757841221ba34eb76292b2d1876a37a7db9
杨宏群 <112977@nd.com>
2019-03-08 17:40:53 +0800

    埃及环境加https
    Change-Id: I951dbba846288e8c733e872680aec44301be4e59

--- login/src/main/java/com/nd/android/adhoc/login/utils/EnvUtils.java
+++ login/src/main/java/com/nd/android/adhoc/login/utils/EnvUtils.java
@@ -26,15 +26,15 @@ public class EnvUtils {
 
     private static void setEgyptEnv() {
         //UC初始化
-        UCManager.getInstance().setBaseUrl("https://101uccenter.mdm.egypt.sdp/v0.93/");
-        UCManager.getInstance().setCSSessionUrl("https://cscommon.mdm.egypt.sdp/v0.1/");
-        UCManager.getInstance().setCaptchaBaseUrl("https://uc-captcha.mdm.egypt.sdp/v0.1/");
-        UCManager.getInstance().setCSBaseUrl("https://egcs.mdm.egypt.sdp/v0.1/");
+        UCManager.getInstance().setBaseUrl("https://101uccenter-mdm.beta.101.com/v0.93/");
+        UCManager.getInstance().setCSSessionUrl("https://cscommon-mdm.beta.101.com/v0.1/");
+        UCManager.getInstance().setCaptchaBaseUrl("https://uc-captcha-mdm.beta.101.com/v0.1/");
+        UCManager.getInstance().setCSBaseUrl("https://egcs-mdm.beta.101.com/v0.1/");
         //CS初始化
 //        CsManager.setContentBaseUrl("http://egcs.mdm.egypt.sdp/v0.1/");
-        CsBaseManager.setContentBaseUrl("https://egcs.mdm.egypt.sdp/v0.1/");
+        CsBaseManager.setContentBaseUrl("https://egcs-mdm.beta.101.com/v0.1/");
 //        CsManager.setContentDownBaseUrl("http://egcs.mdm.egypt.sdp/v0.1/");
-        CsBaseManager.setDownloadBaseUrl("https://egcs.mdm.egypt.sdp/v0.1/");
+        CsBaseManager.setDownloadBaseUrl("https://egcs-mdm.beta.101.com/v0.1/");
 //        Monet.get(getContext()).setKeyGenerator(RedirectKeyGenerator.create().register("pic_fansway", "egcs.mdm.egypt.sdp".split(",")));
     }
 
