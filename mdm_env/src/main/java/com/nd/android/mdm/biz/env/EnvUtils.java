89a5eb02d42333db6553ce6b498bf8133013662f
杨宏群 <112977@nd.com>
2019-03-25 00:45:12 +0800

    埃及正式环境地址修改
    Change-Id: Ie96decabdb486f75fcc03a9e1be1ba0083428597

--- mdm_env/src/main/java/com/nd/android/mdm/biz/env/EnvUtils.java
+++ mdm_env/src/main/java/com/nd/android/mdm/biz/env/EnvUtils.java
@@ -27,15 +27,15 @@ public class EnvUtils {
 
     private static void setEgyptEnv() {
         //UC初始化
-//        UCManager.getInstance().setBaseUrl("https://101uccenter-mdm.beta.101.com/v0.93/");
-//        UCManager.getInstance().setCSSessionUrl("https://cscommon-mdm.beta.101.com/v0.1/");
-//        UCManager.getInstance().setCaptchaBaseUrl("https://uc-captcha-mdm.beta.101.com/v0.1/");
-//        UCManager.getInstance().setCSBaseUrl("https://egcs-mdm.beta.101.com/v0.1/");
+        UCManager.getInstance().setBaseUrl("http://101uccenter-mdm.test.moe.gov.eg/v0.93/");
+        UCManager.getInstance().setCSSessionUrl("http://cscommon-mdm.test.moe.gov.eg/v0.1/");
+        UCManager.getInstance().setCaptchaBaseUrl("http://uc-captcha-mdm.test.moe.gov.eg/v0.1/");
+        UCManager.getInstance().setCSBaseUrl("http://egcs-mdm.test.moe.gov.eg/v0.1/");
         //CS初始化
 //        CsManager.setContentBaseUrl("http://egcs.mdm.egypt.sdp/v0.1/");
-        CsBaseManager.setContentBaseUrl("https://egcs-mdm.beta.101.com/v0.1/");
+        CsBaseManager.setContentBaseUrl("http://egcs-mdm.test.moe.gov.eg/v0.1/");
 //        CsManager.setContentDownBaseUrl("http://egcs.mdm.egypt.sdp/v0.1/");
-        CsBaseManager.setDownloadBaseUrl("https://egcs-mdm.beta.101.com/v0.1/");
+        CsBaseManager.setDownloadBaseUrl("http://egcs-mdm.test.moe.gov.eg/v0.1/");
 //        Monet.get(getContext()).setKeyGenerator(RedirectKeyGenerator.create().register("pic_fansway", "egcs.mdm.egypt.sdp".split(",")));
     }
 
