476beb3904a807adc02ddd668b880f470d073bce
杨宏群 <112977@nd.com>
2019-05-07 19:54:07 +0800

    增加登录成功后保存用户名
    Change-Id: I65212178a4e30565abd301dd160989bb36aadaad

---
+++ loginui/src/main/java/com/nd/android/adhoc/login/ui/BaseLoginActivity.java
@@ -0,0 +1,20 @@
+package com.nd.android.adhoc.login.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.nd.android.adhoc.basic.ui.activity.AdhocBaseActivity;
+
+public abstract class BaseLoginActivity extends AdhocBaseActivity implements ILoginPresenter.IView{
+
+    private ILoginPresenter mPresenter = null;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected abstract ILoginPresenter getPresenter();
+
+
+}
