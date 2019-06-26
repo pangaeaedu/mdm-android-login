e2bcd7cec2606a981a8f414289d0a3ac079b7038
黄艺凯 <807851@nd.com>
2019-06-26 16:33:13 +0800

    1、修正 push 请求订阅可能存在的逻辑问题，改为通过 msgid 是否匹配进行判断 2、receiveFeedback 如果 返回空内容，就不继续往下传，打错误日志
    Change-Id: I2f616fceac9e526e90fdc6f161f5159b96237177

---
+++ adhoc_communicate/src/main/java/com/nd/android/adhoc/communicate/constant/AdhocCommunicateConstant.java
@@ -0,0 +1,10 @@
+package com.nd.android.adhoc.communicate.constant;
+
+/**
+ * Created by HuangYK on 2019/6/26.
+ */
+
+public final class AdhocCommunicateConstant {
+
+    public static final String CMP_NAME = "adhoc_communicate";
+}
