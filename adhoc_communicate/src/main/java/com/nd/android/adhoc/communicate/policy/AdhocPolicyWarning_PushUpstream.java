efed84e70e532a4780693e262daa220d61b1deb1
黄艺凯 <807851@nd.com>
2019-07-09 11:17:56 +0800

    1、增加 push 上行策略对接
    Change-Id: If4df8facf6624b63b949d0658702ce1e260b8126

---
+++ adhoc_communicate/src/main/java/com/nd/android/adhoc/communicate/policy/AdhocPolicyWarning_PushUpstream.java
@@ -0,0 +1,48 @@
+package com.nd.android.adhoc.communicate.policy;
+
+import com.nd.android.adhoc.basic.log.Logger;
+import com.nd.android.adhoc.communicate.impl.MdmTransferConfig;
+import com.nd.android.adhoc.communicate.request.constant.AdhocNetworkChannel;
+import com.nd.android.adhoc.policy.api.AdhocPolicyTaskAbs;
+import com.nd.sdp.android.serviceloader.annotation.Service;
+
+import org.json.JSONObject;
+
+/**
+ * Created by HuangYK on 2019/7/2.
+ */
+@Service(AdhocPolicyTaskAbs.class)
+public class AdhocPolicyWarning_PushUpstream extends AdhocPolicyTaskAbs {
+
+    private static final String TAG = "AdhocPolicyWarning_PushUpstream";
+
+    @Override
+    public int getPolicyType() {
+        return 2;
+    }
+
+    @Override
+    public String getPolicyName() {
+        return "push_upstream";
+    }
+
+
+    @Override
+    public void updateTask(String pPolicyData) {
+        try {
+            JSONObject jsonObject = new JSONObject(pPolicyData);
+
+            // 1 = push上行，0 = http/https，默认 1
+            int enable = jsonObject.optInt("enable", 1);
+
+            MdmTransferConfig.setNetworkChannel(AdhocNetworkChannel.getTypeByValue(enable));
+
+        } catch (Exception e) {
+            Logger.w(TAG, "runTask error: " + e);
+            return;
+        }
+
+        super.updateTask(pPolicyData);
+    }
+
+}
