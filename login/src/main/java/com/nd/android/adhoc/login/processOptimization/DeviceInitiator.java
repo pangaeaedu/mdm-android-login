85135b38095577e235b38f8faca642a33fcee3e1
杨宏群 <112977@nd.com>
2019-08-27 22:22:50 +0800

    刚才QA测试的情况下，出现了push连上有问题，所以DeviceID确认不了的问题。这种情况下，如果出现TimeOutException，我们就重试三次。否则就把异常抛出去。如果超过三次，则扔出一个正式的Timout异常
    Change-Id: I82dc5df53c91f42d258aac570f35d230440f69ff

--- login/src/main/java/com/nd/android/adhoc/login/processOptimization/DeviceInitiator.java
+++ login/src/main/java/com/nd/android/adhoc/login/processOptimization/DeviceInitiator.java
@@ -17,6 +17,8 @@ import com.nd.android.adhoc.login.basicService.data.http.QueryDeviceStatusRespon
 import com.nd.android.adhoc.login.enumConst.ActivateUserType;
 import com.nd.android.adhoc.loginapi.exception.ConfirmIDServerException;
 
+import java.util.concurrent.TimeoutException;
+
 import rx.Observable;
 import rx.Subscriber;
 import rx.Subscription;
@@ -153,6 +155,7 @@ public class DeviceInitiator extends BaseAuthenticator implements IDeviceInitiat
                     } else {
                         deviceID = loadDeviceIDFromPrevSpOrSDCard();
 
+
                         ConfirmDeviceIDResponse result = confirmDeviceIDFromServer(deviceID);
 
                         if(!result.isSuccess()){
@@ -275,9 +278,23 @@ public class DeviceInitiator extends BaseAuthenticator implements IDeviceInitiat
         Log.e("yhq", "input buildSn:"+buildSn+" cpuSn:"+cpuSn+" imei:"+imei
         +" wifiMac:"+wifiMac+" blueToothMac:"+blueToothMac+" serialNo:"+serialNo
         +" androidID:"+androidID+" localDeviceID:"+pLocalDeviceID);
-        ConfirmDeviceIDResponse response =  getHttpService().confirmDeviceID(buildSn, cpuSn, imei,
-                wifiMac, blueToothMac, serialNo, androidID,pLocalDeviceID);
-        Log.e("yhq", "deviceID response:"+response.getDeviceID());
-        return response;
+
+        ConfirmDeviceIDResponse response = null;
+        for (int i = 0; i <= 2; i++) {
+            try {
+                response = getHttpService().confirmDeviceID(buildSn, cpuSn, imei,
+                        wifiMac, blueToothMac, serialNo, androidID, pLocalDeviceID);
+                if (response != null) {
+                    Log.e("yhq", "deviceID response:" + response.getDeviceID());
+                    return response;
+                }
+            } catch (Exception pE) {
+                if(!(pE instanceof TimeoutException)){
+                    throw pE;
+                }
+            }
+        }
+
+        throw new TimeoutException("after retry 3 time, confirm deivce id still timeout");
     }
 }
