f6181289b2f8685eaa3a422039c7e1c36e316107
黄艺凯 <807851@nd.com>
2020-04-14 14:34:56 +0800

    1、根据 OMO 需求，SchoolInfo 新增 distance、schoolName 两个字段
    Change-Id: I49608f2a0ceacdd7562055919713758502377bf9

--- login/src/main/java/com/nd/android/adhoc/login/basicService/http/school/bean/SchoolInfo.java
+++ login/src/main/java/com/nd/android/adhoc/login/basicService/http/school/bean/SchoolInfo.java
@@ -6,6 +6,14 @@ public class SchoolInfo {
     private String governorate;
     private String district;
 
+
+    // 根据 OMO 要求新增
+    // https://pms.101.com/index.php?m=bug&f=view&bugID=493135
+    // https://pms.101.com/index.php?m=bug&f=view&bugID=493071
+    // -- by hyk 20200414
+    private String distance;
+    private String schoolName;
+
     public String getGroupcode() {
         return groupcode;
     }
@@ -20,5 +28,13 @@ public class SchoolInfo {
 
     public String getDistrict() {
         return district;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
     }
 }
