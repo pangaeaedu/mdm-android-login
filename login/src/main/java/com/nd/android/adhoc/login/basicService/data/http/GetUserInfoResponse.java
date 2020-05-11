0fbafb67d9149d668a3b747c892bb3b69f0c2494
黄艺凯 <807851@nd.com>
2020-05-11 16:29:04 +0800

    1、GetUserInfoResponse 增加对 groupcode、groupname、school_name 的解析 2、AssistantSpConfig 增加对 groupcode 的缓存
    Change-Id: I8b9f3e303a9c389aaf810a9208937880e5466407

--- login/src/main/java/com/nd/android/adhoc/login/basicService/data/http/GetUserInfoResponse.java
+++ login/src/main/java/com/nd/android/adhoc/login/basicService/data/http/GetUserInfoResponse.java
@@ -7,6 +7,9 @@ package com.nd.android.adhoc.login.basicService.data.http;
 //        "nick_name":"老王"
 //        "user_id":"xxxxxx"
 //        "device_code": "11" //获取设备编码，需要移动端补齐位数
+//        "groupname":"xxxxx" //组名
+//        "groupcode":"0090" //组code
+//        "school_name":"xxxx学校" //学校名称，如果有配置的话有
 
 public class GetUserInfoResponse {
     public String requestid = "";
@@ -16,9 +19,18 @@ public class GetUserInfoResponse {
     // 设备编码，根据OMO 需求新增的一个字段 -- by hyk 20200410
     public String device_code = "";
 
+    // 根据 OMO 增加对 groupcode 字段的解析，用于后期查询父节点级当前节点的名称等信息 -- by hyk 20200511
+    public String groupcode = "";
+    public String groupname = "";
+    public String school_name = "";
+
+
     public int errcode = 0;
     public int msgcode = 0;
 
+    public void setNick_name(String strNickName){
+        nick_name = strNickName;
+    }
 
     public String getNickName(){
         return nick_name;
@@ -38,5 +50,17 @@ public class GetUserInfoResponse {
 
     public String getDevice_code() {
         return device_code;
+    }
+
+    public String getGroupcode() {
+        return groupcode;
+    }
+
+    public String getGroupname() {
+        return groupname;
+    }
+
+    public String getSchool_name() {
+        return school_name;
     }
 }
