bd5bbbc22b2d2fa9edf8ffcbb07868b5cc0a3150
林胜杰 <585585@nd.com>
2020-12-17 16:07:28 +0800

    monitor改这在登录后开始
    Change-Id: I6a6d0cb81968f2003bee7c7787b41cdb7e3eb7ea
    
    (cherry picked from commit dd3485e)

---
+++ mdm_monitor/src/main/java/com/nd/android/mdm/monitor/initialize/MonitorLoginListenerImpl.java
@@ -0,0 +1,30 @@
+package com.nd.android.mdm.monitor.initialize;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.nd.android.adhoc.basic.common.AdhocBasicConfig;
+import com.nd.android.adhoc.basic.frame.api.user.IAdhocLoginInfo;
+import com.nd.android.adhoc.basic.frame.api.user.IAdhocLoginListener;
+import com.nd.android.adhoc.basic.log.Logger;
+import com.nd.android.mdm.monitor.MonitorModule;
+import com.nd.sdp.android.serviceloader.annotation.Service;
+
+/**
+ * Created by HuangYK on 2018/12/6.
+ */
+@Service(IAdhocLoginListener.class)
+public class MonitorLoginListenerImpl implements IAdhocLoginListener {
+
+    private static final String TAG = "MonitorLoginListenerImpl";
+
+    @Override
+    public void onLogin(@NonNull IAdhocLoginInfo pLoginInfo) {
+        try {
+            Context context = AdhocBasicConfig.getInstance().getAppContext();
+            MonitorModule.getInstance().init(context);
+        } catch (Exception e) {
+            Logger.e(TAG, "onLogin exception:" + e);
+        }
+    }
+}
