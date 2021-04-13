32a50840735d9869b89d66251faa00c5fcd57962
黄艺凯 <807851@nd.com>
2021-04-13 23:57:37 +0800

    补充 LoginApiRoutePathConstants，并修改了 ROOT 的路径
    Change-Id: I137c6fa28417a4096e7ad2cb9ed6c7e1d913f1c3

---
+++ device_status/device_activate_biz_api/src/main/java/com/nd/android/aioe/device/activate/biz/api/constant/LoginApiRoutePathConstants.java
@@ -0,0 +1,13 @@
+package com.nd.android.aioe.device.activate.biz.api.constant;
+
+public class LoginApiRoutePathConstants {
+
+    public static final String PATH_LOGINAPI_ROOT = "/activate_biz_api/";
+
+    public static final String PATH_LOGINAPI_LOGINUI = PATH_LOGINAPI_ROOT + "loginui";
+
+    public static final String PATH_LOGINAPI_INIT = PATH_LOGINAPI_ROOT + "init";
+
+    public static final String PATH_LOGINAPI_LOGIN = PATH_LOGINAPI_ROOT + "login";
+
+}
