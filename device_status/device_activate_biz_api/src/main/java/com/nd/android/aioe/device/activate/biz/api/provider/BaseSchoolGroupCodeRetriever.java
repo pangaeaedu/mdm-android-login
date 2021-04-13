c67103adf534dfb05116beaeca3676f2a0d1aab5
黄艺凯 <807851@nd.com>
2021-04-13 16:51:33 +0800

    补充 BaseSchoolGroupCodeRetriever
    Change-Id: I5e44add45660d6aea7783c208e01f3aa8348cc99

---
+++ device_status/device_activate_biz_api/src/main/java/com/nd/android/aioe/device/activate/biz/api/provider/BaseSchoolGroupCodeRetriever.java
@@ -0,0 +1,64 @@
+package com.nd.android.aioe.device.activate.biz.api.provider;
+
+import android.support.annotation.NonNull;
+
+import com.nd.android.adhoc.basic.log.Logger;
+
+import java.util.concurrent.CountDownLatch;
+
+public abstract class BaseSchoolGroupCodeRetriever implements ISchoolGroupCodeRetriever {
+
+    private static final String TAG = "GroupCodeRetriever";
+    private final CountDownLatch mCountDownLatch = new CountDownLatch(1);
+    private String mGroupCode = null;
+
+
+    @NonNull
+    @Override
+    public String retrieveGroupCode(String pRootCode) throws Exception {
+
+        // TODO： 上层自行去限制
+//        boolean isXiaomi = AdhocRomFactory.getInstance().getRomStrategy() instanceof AdhocRomStrategy_Xiaomi;
+//
+//        Logger.d(TAG, "Settings.Global.DEVICE_PROVISIONED value = " + Settings.Global.getInt(AdhocBasicConfig.getInstance().getAppContext().getContentResolver(), Settings.Global.DEVICE_PROVISIONED, 0));
+//        while (isXiaomi) {
+//            if (Settings.Global.getInt(AdhocBasicConfig.getInstance().getAppContext().getContentResolver(), Settings.Global.DEVICE_PROVISIONED, 0) == 1) {
+//                break;
+//            }
+//            Thread.sleep(3 * 1000);
+//        }
+
+        showUI(pRootCode);
+        try {
+            mCountDownLatch.await();
+        } catch (Exception ignored) {
+
+        }
+        return mGroupCode;
+    }
+
+    @NonNull
+    @Override
+    public String onGroupNotFound(String pRootCode) throws Exception {
+        groupNotFound(pRootCode);
+        try {
+            mCountDownLatch.await();
+        } catch (Exception ignored) {
+
+        }
+        return mGroupCode;
+    }
+
+    protected abstract void showUI(String pRootCode);
+
+    protected abstract void groupNotFound(String pRootCode);
+
+    protected void setGroupCode(String sGroupCode) {
+        Exception e = new Exception("this is a log");
+        e.printStackTrace();
+
+        Logger.i(TAG, "setGroupCode:" + sGroupCode);
+        mGroupCode = sGroupCode;
+        mCountDownLatch.countDown();
+    }
+}
